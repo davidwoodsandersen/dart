265a64afa19bf9107987d61818ad686c8a2b229c
David Andersen <davidwoodsandersen@gmail.com>
2018-04-19 00:54:11 -0400

    adds more video class tests

--- tests/modules/video.test.js
+++ tests/modules/video.test.js
@@ -1,4 +1,3 @@
-// import jest from 'jest';
 import Video from '../../src/modules/video.js';
 import testLinks from '../../test-links.json';
 
@@ -20,6 +19,20 @@ test('Video.init sets the element "src" attribute to props.source', () => {
 	expect(video.element.getAttribute('src')).toEqual(testLinks.video);
 });
 
+test('Video.init anchors the video player in the DOM', () => {
+	var video = new Video({
+		source: testLinks.video,
+		parent: document.body
+	});
+
+	jest.spyOn(HTMLElement.prototype, 'appendChild')
+		.mockImplementation(() => {});
+
+	video.init();
+
+	expect(HTMLElement.prototype.appendChild).toHaveBeenCalledTimes(1);
+});
+
 test('Video.play plays the video file', () => {
 	var video = new Video({
 		source: testLinks.video,
@@ -33,4 +46,23 @@ test('Video.play plays the video file', () => {
 	video.play();
 
 	expect(HTMLMediaElement.prototype.play).toHaveBeenCalledTimes(1);
+});
+
+test('Video.pause pauses the video file', () => {
+	var video = new Video({
+		source: testLinks.video,
+		parent: document.body
+	});
+
+	jest.spyOn(HTMLMediaElement.prototype, 'play')
+		.mockImplementation(() => {});
+
+	jest.spyOn(HTMLMediaElement.prototype, 'pause')
+		.mockImplementation(() => {});
+
+	video.init();
+	video.play();
+	video.pause();
+
+	expect(HTMLMediaElement.prototype.pause).toHaveBeenCalledTimes(1);
 });
