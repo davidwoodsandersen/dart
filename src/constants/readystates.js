f54b57b0440978b0dc52ba8aba4733da4f9d2554
David Andersen <davidwoodsandersen@gmail.com>
2018-05-06 14:53:03 -0400

    adds ready state aliases

---
+++ src/constants/readystates.js
@@ -0,0 +1,13 @@
+/**
+ * @constant readyStates
+ * @type {object}
+ * @description Aliases for the video element ready states.
+ */
+
+export default {
+	HAVE_ENOUGH_DATA: 4,
+	HAVE_FUTURE_DATA: 3,
+	HAVE_CURRENT_DATA: 2,
+	HAVE_METADATA: 1,
+	HAVE_NOTHING: 0
+};
